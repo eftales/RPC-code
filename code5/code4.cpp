8d8268bc8997e7d6a697e857c073666351e006e8
eftales <1479803084@qq.com>
2019-11-01 15:44:24 +0800

    简化了代码，增加了注释

--- code5/code4.cpp
+++ code5/code4.cpp
@@ -28,16 +28,16 @@ public:
         result = 0;
     }
 
-    int start(int a, int b) {
+    int start(int a, int b) { // 整个流程为：传入两个整形数据，发送给服务端，服务端再返回一个数据。
         boost::system::error_code ec;
-        socket_.connect(endpoint_, ec);
+        socket_.connect(endpoint_, ec); // 连接服务端
         if (!ec)
         {
             static tcp::no_delay option(true);
-            socket_.set_option(option);
+            socket_.set_option(option); // 设置 socket 为无时延 socket
 
-            construct_rpc_data( a , b);
-            send_recive_rpc_data(ec);
+            construct_rpc_data( a , b); // 构造发送给服务端的数据，包含包的长度和序列化之后的数据。整个包存储在成员变量 buffer 里面
+            send_recive_rpc_data(ec); // 接受来自服务端的信息，存储在成员变量 result 里面
             std::cout << "send_recive_rpc_data返回值：" << result << std::endl;
             return result;
         }
@@ -50,10 +50,9 @@ public:
     }
 
 private:
-    void construct_rpc_data(int a, int b)
+    void construct_rpc_data(int a, int b) // 构造 msgpack 包，获取 msgpack 的长度，将以上两个信息写入成员变量 buffer
     {
 
-//#define protocol_to_client std::tuple<int>
         std::tuple<int, int>  src(1,2);
         std::stringstream sbuffer;
         msgpack::pack(sbuffer, src);
@@ -64,20 +63,21 @@ private:
         memcpy(buffer->data(), &len_bigend, 4);
         memcpy(buffer->data() + 4, strbuf.data(), strbuf.size());
     }
-    void send_recive_rpc_data(const boost::system::error_code& error) {
+    void send_recive_rpc_data(const boost::system::error_code& error) // 将数据发送给服务端，并接受来自服务端的信息
+    {
 
         auto self = this->shared_from_this();
         auto async_buffer = buffer;
 
 
-        boost::asio::async_write(socket_, boost::asio::buffer(*async_buffer, MAXPACKSIZE),
+        boost::asio::async_write(socket_, boost::asio::buffer(*async_buffer, MAXPACKSIZE), // 异步发送数据
             [this,self, async_buffer](const boost::system::error_code& ec, std::size_t size)
             {
-                recive_rpc_data(ec);
+                recive_rpc_data(ec); // 接收数据
 
                 io_service_.stop();
             });
-        io_service_.run();
+        io_service_.run(); // 同步等待上述事件完成
 
     }
 
@@ -91,11 +91,11 @@ private:
             {
 
                 std::cout << "数据读取完成" << std::endl;
-                handle_rpc_data(ec);
-                io_service_.stop();
+                handle_rpc_data(ec); // 对读到的数据进行处理
+                io_service_.stop(); // 终止 asio 事件循环
 
             });
-        io_service_.run();
+        io_service_.run(); // 同步等待上述时间完成
 
 
     }
@@ -122,11 +122,11 @@ typedef boost::shared_ptr<client> client_ptr;
 
 int main()
 {
-    boost::asio::io_service io_service;
-    tcp::endpoint endpoint(address::from_string("127.0.0.1"), 2019);
+    boost::asio::io_service io_service; // 定义 io_service
+    tcp::endpoint endpoint(address::from_string("127.0.0.1"), 2019); // 定义远端地址
 
-    client_ptr new_session(new client(io_service, endpoint));
-    new_session->start(1,2);
+    client_ptr new_session(new client(io_service, endpoint)); // 初始化 client 类，与服务器建立连接
+    new_session->start(1,2); // 向服务端发送数据
     io_service.run();
 
     return 0;
